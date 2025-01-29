b158d32a7f9276cfafca272b02b31ca6a4880433
Matheus Alves <matheussilvaemac@gmail.com>
2025-01-29 15:16:38 -0300

    repetições em Java Script

---
+++ aula13/modelo.js
@@ -0,0 +1,17 @@
+//Repetição com While - teste lógico e depois executa
+
+let contagem = 1
+while (contagem <=4){
+    console.log(`${contagem}`)
+    contagem++
+} console.log('acabou')
+
+//Repetição com do - execução e depois teste lógico
+
+let c = 1
+
+do{
+    console.log('oi, viado')
+    c++
+} while (c <=4) 
+    console.log('acabou também')
---
+++ aula14/modelo.js
@@ -0,0 +1,19 @@
+// Repetição com variável de controle
+
+for(var contagem = 1; contagem <= 10; contagem++){
+    console.log(contagem)
+}
+
+/*
+For(início, teste, incremento)
+
+início => é a variável ou o dado
+teste => é o que você colocou como referência da 
+repetição (número de repetições)
+incremento => como a variável vai avançar (c++ = de um em um)
+
+Nesse método o código, ao terminar, caso não
+chegue ao número de repetições ele executa
+o comando, adiciona mais um incremento e
+retorna ao teste
+*/ 
